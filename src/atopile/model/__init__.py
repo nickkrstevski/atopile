ee2cc9acd178614bc8726845a320c898c10925f2
Matt Wildoer <mattwildoer@Matts-MacBook-Air.local>
2023-04-21 22:59:13 -0700

    refactor model directory

---
+++ src/atopile/model/__init__.py
@@ -0,0 +1,3 @@
+from .find import find_root_vertex, find_vertex_at_path, ancestory_dot_com, whos_your_daddy
+from .model import add_block
+from .plot import plot
--- src/atopile/model/find.py
+++ src/atopile/model/find.py
@@ -1,19 +1,4 @@
 import igraph as ig
-from typing import Optional
-
-def plot(g: ig.Graph, *args, **kwargs):
-    color_dict = {
-        "block": "red",
-        "package": "green",
-        "pin": "cyan",
-        "ethereal_pin": "magenta",
-        "connects_to": "blue",
-        "part_of": "black",
-    }
-    kwargs["vertex_color"] = [color_dict.get(type_name, "grey") for type_name in g.vs["type"]]
-    kwargs["vertex_label"] = g.vs["ref"]
-    kwargs["edge_color"] = [color_dict[type_name] for type_name in g.es["type"]]
-    return ig.plot(g, *args, **kwargs)
 
 def find_vertex_at_path(g: ig.Graph, path: str):
     path_parts = path.split('.')
@@ -22,7 +7,7 @@ def find_vertex_at_path(g: ig.Graph, path: str):
         raise ValueError(f"Multiple verticies found at path {path_parts}. Graph is invalid")
     for ref in path_parts:
         candidates = ig.VertexSeq(g, {i.index for c in candidates for i in c.neighbors(mode='in')})
-        candidates = candidates.find(ref_eq=ref)
+        candidates = candidates.select(ref_eq=ref)
     return candidates[0]
 
 def find_root_vertex(g: ig.Graph):
@@ -30,16 +15,6 @@ def find_root_vertex(g: ig.Graph):
     if len(candidates) > 1:
         raise ValueError("Multiple root verticies found. Graph is invalid")
     return candidates[0]
-
-def add_block(g: ig.Graph, block: ig.Graph, block_ref: str, parent: Optional[str] = None):
-    block_start_index = len(g.vs)
-    block_root_index = find_root_vertex(block).index + block_start_index
-    g += block
-    g.vs[block_root_index]['ref'] = block_ref
-
-    if parent:
-        g.add_edge(block_root_index, find_vertex_at_path(g, parent).index, type='part_of')
-    return g
 
 def ancestory_dot_com(g: ig.Graph, v: int):
     """
