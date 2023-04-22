713638a2ebe2271a2741d6df4e0472b6c23e58d7
Matt Wildoer <mattwildoer@Matts-MacBook-Air.local>
2023-04-21 21:39:36 -0700

    create functions to find a vertex from a path, find a root vertex and add a block

--- sandbox/model3.py
+++ sandbox/model3.py
@@ -25,14 +25,14 @@ def plot(g: ig.Graph):
         "ethereal_pin": "magenta",
         "connects_to": "blue",
         "part_of": "black",
-        None: "grey",
     }
     visual_style = {}
-    visual_style["vertex_color"] = [color_dict[type_name] for type_name in g.vs["type"]]
+    visual_style["vertex_color"] = [color_dict.get(type_name, "grey") for type_name in g.vs["type"]]
     visual_style["vertex_label"] = g.vs["ref"]
     visual_style["edge_color"] = [color_dict[type_name] for type_name in g.es["type"]]
     return ig.plot(g, **visual_style)
 
+#%%
 # eg.;
 
 resistor = ig.Graph(directed=True)
@@ -44,54 +44,41 @@ resistor.add_vertices(
 resistor.add_edges([(0, 5), (1, 5), (2, 4), (3, 4), (4, 5)], {'type': ['part_of'] * 5})
 resistor.add_edges([(0, 2), (1, 3)], {'type': ['connects_to'] * 2})
 
-plot(resistor)
-
-# %%
-g = ig.Graph(directed=True)
-plot(g)
+# plot(resistor)
 
 #%%
 def find_vertex_at_path(g: ig.Graph, path: str):
-    path = path.split('.')
-    candidates = g.vs.select(ref_eq=path.pop(0))
-    for ref in path:
-        candidates = ig.VertexSeq(g, [i for c in resistor.vs for i in c.neighbors(mode='in')])
-        candidates = g.vs.select(ref_eq=ref)
+    path_parts = path.split('.')
+    candidates = g.vs.select(ref_eq=path_parts.pop(0))
     if len(candidates) > 1:
-        raise ValueError(f"Multiple verticies found at path {path}")
+        raise ValueError(f"Multiple verticies found at path {path_parts}. Graph is invalid")
+    for ref in path_parts:
+        candidates = ig.VertexSeq(g, {i.index for c in candidates for i in c.neighbors(mode='in')})
+        candidates = candidates.find(ref_eq=ref)
     return candidates[0]
-#%%
+
+def find_root_vertex(g: ig.Graph):
+    candidates = g.vs.select(type_eq='block', _outdegree_eq=0)
+    if len(candidates) > 1:
+        raise ValueError("Multiple root verticies found. Graph is invalid")
+    return candidates[0]
 
 def add_block(g: ig.Graph, block: ig.Graph, block_ref: str, parent: Optional[str] = None):
-    block_index = len(g.vs)
+    block_start_index = len(g.vs)
+    block_root_index = find_root_vertex(block).index + block_start_index
     g += block
-    g.vs[block_index]['ref'] = block_ref
+    g.vs[block_root_index]['ref'] = block_ref
+
     if parent:
-        g.vs[block_index]['path'] = parent + block_ref
-        g.add_edge(block_index, g.vs.find(path_eq=parent).index, {'type': 'part_of'})
+        g.add_edge(block_root_index, find_vertex_at_path(g, parent).index, type='part_of')
+    return g
 
-
-# add_block(g, resistor, 'resistor', 'r1')
-
-
-# %%
-plot(g.subgraph(g.vs.select(type_in=['pin', 'ethereal_pin'])))
-
-# %%
-resistor.vs['ref']
-# %%
-g.vs['ref']
-
-# %%
-vs = g.vs.select(type_in=['pin', 'ethereal_pin'])
-vs.select(ref_eq='1')
-# %%
-ig.plot(resistor.connected_components(mode='WEAK').subgraphs())
 #%%
-"path.to.something".split('.')[1::-1]
-# %%
-[i for c in resistor.vs for i in c.neighbors(mode='in')]
+g = ig.Graph(directed=True)
+g.add_vertex(ref='root', type='block')
+g = add_block(g, resistor, 'r1', 'root')
+g = add_block(g, resistor, 'r2', 'root')
+# g.add_edges([(1, 7), (2, 8), (13, 19), (14, 20)], {'type': ['connects_to'] * 2})
+plot(g)
 
-# %%
-find_vertex_at_path(g, 'r1.resistor.1')
 # %%
