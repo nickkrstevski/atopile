ce6ddeea61463d71d1c14f53af938bc0f09699d6
Timot05 <timothee.peter@mac.com>
2023-04-29 11:49:02 -0700

    add create vertex function

--- sandbox/model2.py
+++ sandbox/model2.py
@@ -10,61 +10,73 @@ from atopile.netlist.graph_to_netlist import generate_component_list_from_graph
 
 v_div = voltage_divider
 res = resistor
+
+res = model.add_vertex(res, 'testing', 'block')
+
+res = model.add_vertex(res, 'test2', 'ethereal_pin', 'testing')
+
+model.plot(res)
+
+# %%
+
+res = model.instantiate(res, 'testing', 'copied', 'testing')
+model.plot(res)
+# %%
 #nets = generate_nets_dict_from_graph(v_div)
 
 #print(nets)
 
-print(generate_component_list_from_graph(v_div))
-model.plot(res)
+# print(generate_component_list_from_graph(v_div))
+# model.plot(res)
 
-# %%
+# # %%
 
-s_graph = starter_graph
-model.plot(s_graph)
-# %%
-result = model.instantiate_from_with_ref(s_graph, 'b.e', 'test')
-#result = model.find_vertex_at_path(s_graph, 'starter_block.starter_ethereal_pin')
-model.plot(result)
-# %%
+# s_graph = starter_graph
+# model.plot(s_graph)
+# # %%
+# result = model.instantiate_from_with_ref(s_graph, 'b.e', 'test')
+# #result = model.find_vertex_at_path(s_graph, 'starter_block.starter_ethereal_pin')
+# model.plot(result)
+# # %%
 
-# %%
-import igraph
+# # %%
+# import igraph
 
-# create the original DAG
-g = igraph.Graph(directed=True)
-g.add_vertices(
-    3,
-    {'type': ['ethereal_pin'] + ['pin'] + ['block'],
-     'ref': ['1', '2', '3']}
-)
-g.add_edges([(0, 1), (1, 2)], {'type': ['connects_to'] * 2})
+# # create the original DAG
+# g = igraph.Graph(directed=True)
+# g.add_vertices(
+#     3,
+#     {'type': ['ethereal_pin'] + ['pin'] + ['block'],
+#      'ref': ['1', '2', '3']}
+# )
+# g.add_edges([(0, 1), (1, 2)], {'type': ['connects_to'] * 2})
 
-model.plot(g)
-# %%
+# model.plot(g)
+# # %%
 
-# choose a vertex as the starting point for the subgraph
-v = 2
+# # choose a vertex as the starting point for the subgraph
+# v = 2
 
-res = model.instantiate_from_with_ref(res, v, 'ref')
-# # find all vertices and edges below the starting vertex
-# subgraph_vertices = g.subcomponent(v, mode="out")
-# print(subgraph_vertices)
+# res = model.instantiate_from_with_ref(res, v, 'ref')
+# # # find all vertices and edges below the starting vertex
+# # subgraph_vertices = g.subcomponent(v, mode="out")
+# # print(subgraph_vertices)
 
-# h = igraph.Graph(directed=True)
-# h = g.subgraph(subgraph_vertices)
-model.plot(res)
-# %%
-# create the subgraph
-g = model.add_block(g, h, 'test', '1') 
+# # h = igraph.Graph(directed=True)
+# # h = g.subgraph(subgraph_vertices)
+# model.plot(res)
+# # %%
+# # create the subgraph
+# g = model.add_block(g, h, 'test', '1') 
 
 
 
-# add the subgraph to the new graph
-#h = g.compose(subgraph)
+# # add the subgraph to the new graph
+# #h = g.compose(subgraph)
 
-# add an edge from vertex 1 in the new graph to the first vertex of the subgraph
-#h.add_edges([(0, len(subgraph_vertices))])
-print(h)
-model.plot(g)
+# # add an edge from vertex 1 in the new graph to the first vertex of the subgraph
+# #h.add_edges([(0, len(subgraph_vertices))])
+# print(h)
+# model.plot(g)
 
 # %%
--- src/atopile/model/model.py
+++ src/atopile/model/model.py
@@ -13,20 +13,70 @@ def add_block(g: ig.Graph, block: ig.Graph, block_ref: str, parent: Optional[str
         g.add_edge(block_root_index, find_vertex_at_path(g, parent).index, type='part_of')
     return g
 
-def add_graph(g: ig.Graph, added_graph: ig.Graph, root_ref: str, parent: str):
+def add_graph(g: ig.Graph, graph_to_add: ig.Graph, graph_ref: str, parent: Optional[str] = None):
+    graph_start_index = len(g.vs)
+    graph_root_index = find_root_vertex(graph_to_add).index + graph_start_index
+    g += graph_to_add
+    g.vs[graph_root_index]['ref'] = graph_ref
+
+    print('the vertex is', find_vertex_at_path(g, parent).index)
+
+    if parent:
+        g.add_edge(graph_root_index, find_vertex_at_path(g, parent).index, type='part_of')
+
+    g.add_edge(graph_root_index, )
+
+    return g
+
+def add_vertex(g: ig.Graph, ref: str, type: str, parent: Optional[str] = None):
     start_index = len(g.vs)
-    root_index = find_root_vertex(added_graph).index + start_index
-    g += added_graph
-    g.vs[root_index]['ref'] = root_ref
+    vertex_index = start_index
+    
+    g.add_vertex()
+    g.vs[vertex_index]['ref'] = ref
+    g.vs[vertex_index]['type'] = type
 
-    g.add_edge(root_index, find_vertex_at_path(g, parent).index, type='part_of')
+    if parent:
+        g.add_edge(vertex_index, find_vertex_at_path(g, parent).index, type='class_of')
 
-    return added_graph
+    return g
+
+def instantiate(g: ig.Graph, path_to_copy: str, ref_copied: str, parent: Optional[str] = None):
+    
+    # Find the vertex for the class to be instantiated
+    vertex_to_instantiate = find_vertex_at_path(g, path_to_copy)
+
+    # Find all the vertices below that class
+    subgraph_vertices = g.subcomponent(vertex_to_instantiate.index, mode="in")
+    
+    # Create two subhraphs, one for the graph below the class and the other for the completed graph
+    sub_g = ig.Graph(directed=True)
+    completed_g = ig.Graph(directed=True)
+
+    # Create a 
+    sub_g = g.subgraph(subgraph_vertices)
+
+    graph_start_index = len(g.vs)
+    graph_root_index = find_root_vertex(graph_to_add).index + graph_start_index
+    g += graph_to_add
+    g.vs[graph_root_index]['ref'] = graph_ref
+
+    print('the vertex is', find_vertex_at_path(g, parent).index)
+
+    if parent:
+        g.add_edge(graph_root_index, find_vertex_at_path(g, parent).index, type='part_of')
+
+    g.add_edge(graph_root_index, )
+
+    completed_g = add_graph(g, sub_g, ref_copied, parent)
+    
+    return completed_g
 
 def instantiate_from_with_ref(g: ig.Graph, path: str, ref: str):
     vertex_to_instantiate = find_vertex_at_path(g, path)
     
     subgraph_vertices = g.subcomponent(vertex_to_instantiate.index, mode="in")
+    
 
     sub_g = ig.Graph(directed=True)
     completed_g = ig.Graph(directed=True)
