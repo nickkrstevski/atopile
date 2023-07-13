97e8e16d03aac06da8cb0a78fb24e04832c56a7d
Matt Wildoer <mattwildoer@Matts-MacBook-Air.local>
2023-07-13 09:01:58 -0700

    add xfail test-case to draft flipped naming tests

--- tests/targets/netlist/test_resolve_net_name.py
+++ tests/targets/netlist/test_resolve_net_name.py
@@ -1,28 +1,30 @@
-from pytest import mark
+import pytest
+
 from atopile.model.model import Model
-from atopile.targets.netlist.nets import resolve_abs_name, find_nets, find_net_names
+from atopile.targets.netlist.nets import find_net_names, find_nets, resolve_abs_name
 
 expected_nets = {
     "dummy_file.ato:dummy_module.dummy_comp0.sig0-sig1": [
         "dummy_file.ato:dummy_module.dummy_comp0.sig0",
         "dummy_file.ato:dummy_module.dummy_comp0.sig1",
         "dummy_file.ato:dummy_module.dummy_comp0.p0",
-        "dummy_file.ato:dummy_module.dummy_comp0.p1"
+        "dummy_file.ato:dummy_module.dummy_comp0.p1",
     ],
     "dummy_file.ato:dummy_module.dummy_comp1.sig0": [
         "dummy_file.ato:dummy_module.dummy_comp1.sig0",
-        "dummy_file.ato:dummy_module.dummy_comp1.p0"
+        "dummy_file.ato:dummy_module.dummy_comp1.p0",
     ],
     "dummy_file.ato:dummy_module.top_sig": [
         "dummy_file.ato:dummy_module.dummy_comp1.sig1",
         "dummy_file.ato:dummy_module.dummy_comp1.p1",
-        "dummy_file.ato:dummy_module.top_sig"
+        "dummy_file.ato:dummy_module.top_sig",
     ],
     "dummy_file.ato:dummy_module.dummy_comp0.spare_sig-dummy_comp1.spare_sig": [
         "dummy_file.ato:dummy_module.dummy_comp0.spare_sig",
         "dummy_file.ato:dummy_module.dummy_comp1.spare_sig",
     ],
 }
+
 
 def test_find_nets(dummy_model: Model):
     nets = find_nets(dummy_model)
@@ -41,9 +43,11 @@ def test_find_nets(dummy_model: Model):
 
     assert len(checked_nets) == len(expected_nets)
 
-@mark.xfail
+
+@pytest.mark.xfail
 def test_nothing():
     raise NotImplementedError
+
 
 def test_naming(dummy_model: Model):
     nets = find_nets(dummy_model)
@@ -51,9 +55,16 @@ def test_naming(dummy_model: Model):
 
     assert net_names == set(expected_nets.keys())
 
+
 def test_find_net_names(dummy_model: Model):
     net_names = find_net_names(dummy_model)
     assert set(net_names.keys()) == set(expected_nets.keys())
 
     for name, net in net_names.items():
         assert {v.path for v in net} == set(expected_nets[name])
+
+
+# TODO:
+@pytest.mark.xfail
+def test_flipped_naming():
+    raise NotImplementedError
