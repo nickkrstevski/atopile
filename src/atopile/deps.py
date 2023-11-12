c5dc4cadbb9242b2249f7c8042a23d1142e233ee
Matthew Wildoer <mawildoer@gmail.com>
2023-11-12 15:45:31 -0800

    not sure we need this anymore, but sabing tweaks

--- src/atopile/deps.py
+++ src/atopile/deps.py
@@ -5,7 +5,7 @@ from itertools import chain
 from atopile.model2 import errors
 from atopile.parser.AtopileParserVisitor import AtopileParserVisitor
 from atopile.parser.AtopileParser import AtopileParser as ap
-from atopile.parser.parser2 import ParserRuleContext
+from atopile.model2.parse import ParserRuleContext
 from pathlib import Path
 
 log = logging.getLogger(__name__)
@@ -128,7 +128,7 @@ class DependencySolver(typing.Generic[T]):
             if not buildable:
                 leftovers = set(self._dependency_tree) - complete
                 friendly_leftovers = ", ".join(map(str, leftovers))
-                raise errors.AtoCompileError(
+                raise errors.AtoError(
                     "We were unable to find a way to build everything."
                     f" {friendly_leftovers} remains un-built."
                 )
