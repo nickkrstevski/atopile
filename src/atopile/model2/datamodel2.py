d652c60ba0b137bb1eaf5595329e481bec52f967
Matthew Wildoer <mawildoer@gmail.com>
2023-11-13 22:52:16 -0800

    Working dm1 name resolution

--- src/atopile/model2/datamodel2.py
+++ src/atopile/model2/datamodel2.py
@@ -18,7 +18,7 @@ import collections
 from pathlib import Path
 from typing import Any, Iterable, Optional, Mapping
 
-from attrs import define, resolve_types
+from attrs import define, resolve_types, field
 
 from atopile.model2 import datamodel1 as dm1
 from atopile.model2 import errors
@@ -28,7 +28,12 @@ log.setLevel(logging.INFO)
 
 
 @define
-class Link:
+class Base:
+    src: Optional[dm1.Base] = field(default=None, kw_only=True, eq=False)
+
+
+@define
+class Link(Base):
     start_instance: "Object"
     start_node: "Object"
     end_instance: "Object"
@@ -36,18 +41,18 @@ class Link:
 
 
 @define
-class Replace:
+class Replace(Base):
     original: "Object"
     replacement: "Object"
 
 
 @define
-class Import:
+class Import(Base):
     what: "Object"
 
 
 @define
-class Object:
+class Object(Base):
     # these are populated by Scoop
     locals_: Optional[Mapping] = None
     supers: Optional[tuple["Object"]] = None
@@ -55,6 +60,7 @@ class Object:
     # these are created by Scoop, for the next guy
     name_bindings: Optional[Mapping] = None
     closure: Optional[Mapping] = None
+
 
 for cls in [Object, Link, Replace, Import]:
     resolve_types(cls)
@@ -70,11 +76,11 @@ INTERFACE = Object()
 
 
 BUILTINS = {
-    dm1.MODULE[-1][0]: MODULE,
-    dm1.COMPONENT[-1][0]: COMPONENT,
-    dm1.PIN[-1][0]: PIN,
-    dm1.SIGNAL[-1][0]: SIGNAL,
-    dm1.INTERFACE[-1][0]: INTERFACE,
+    dm1.MODULE[-1]: MODULE,
+    dm1.COMPONENT[-1]: COMPONENT,
+    dm1.PIN[-1]: PIN,
+    dm1.SIGNAL[-1]: SIGNAL,
+    dm1.INTERFACE[-1]: INTERFACE,
 }
 
 
@@ -83,8 +89,13 @@ class Scoop:
     Scoop's job is to walk the tree of dm1 objects, creating
     Frames and empty dm2 analog Objects for each dm1 object
     """
+
     def __init__(self) -> None:
-        self.dm1_id_object_map: dict[int, Object] = collections.defaultdict(Object)
+        self.builtins_map = collections.ChainMap(BUILTINS)
+
+        self.dm1_id_object_map: dict[int, Object] = {
+            id(obj): obj for obj in self.builtins_map.values()
+        }
         self.dm1_id_name_binding_map: dict[int, dict[str, Any]] = {}
         self.dm1_id_closure_map: dict[int, collections.ChainMap[dm1.Ref, Any]] = {}
         self.collected_errors: list[errors.AtoError] = []
@@ -94,31 +105,35 @@ class Scoop:
 
     def visit(self, dm1_obj: dm1.Object, closure: collections.ChainMap) -> None:
         assert id(dm1_obj) not in self.dm1_id_object_map
-        self.dm1_id_closure_map[id(dm1_obj)] = Object()
-        binding_map = self.dm1_id_name_binding_map[id(dm1_obj)] = dict(filter(lambda x: x[0] is not None, dm1_obj.locals_))
+
+        self.dm1_id_object_map[id(dm1_obj)] = Object(src=dm1_obj)
+        binding_map = self.dm1_id_name_binding_map[id(dm1_obj)] = dict(
+            filter(lambda x: x[0] is not None, dm1_obj.locals_)
+        )
         self.dm1_id_closure_map[id(dm1_obj)] = closure
 
         child_closure = closure.new_child(binding_map)
         for _, child in dm1_obj.locals_:
-            if not isinstance(child, dm1.Object):
-                continue
-            self.visit(child, child_closure)
+            if isinstance(child, dm1.Object):
+                self.visit(child, child_closure)
 
     def visit_roots(self, objs: Iterable[dm1.Object]) -> None:
         """
         Expected to be called on an iterable of file objects, which are generally the roots of these trees
         """
-        builtins_map = collections.ChainMap(BUILTINS)
         for obj in objs:
-            self.visit(obj, builtins_map)
+            self.visit(obj, self.builtins_map)
         if self.collected_errors:
-            raise ExceptionGroup("Errors occurred while visiting roots", self.collected_errors)
+            raise ExceptionGroup(
+                "Errors occurred while visiting roots", self.collected_errors
+            )
 
 
 class Lofty:
     """
     Lofty's job is to walk the tree, filling in the dm2 objects
     """
+
     def __init__(
         self,
         dm1_id_object_map: dict[int, Object],
@@ -153,19 +168,58 @@ class Lofty:
                 return candidate_path.resolve().absolute()
         raise errors.AtoImportNotFoundError(f"Could not find import: {dep_name}")
 
-    def lookup_dm2_obj_by_ref(self, dm1_obj: dm1.Object, ref: dm1.Ref) -> Object:
+    def find_inside_mapping_dm1(self, ref: dm1.Ref, dm1_obj: dm1.Object, mapping: Mapping) -> Any:
+        if not ref:
+            return dm1_obj
+
+        try:
+            return mapping[ref]
+        except KeyError:
+            pass
+
+        for i in range(1, len(ref)):
+            ref_fragment = ref[:-i]
+            remaining_ref = ref[-i:]
+            try:
+                matched_obj = mapping[ref_fragment]
+                if not isinstance(matched_obj, dm1.Object):
+                    raise TypeError
+
+                return self.find_inside_dm1(
+                    remaining_ref,
+                    matched_obj,
+                )
+            except KeyError:
+                continue
+        raise KeyError
+
+    def find_inside_dm1(self, ref: dm1.Ref, dm1_obj: dm1.Object) -> Any:
+        if not ref:
+            return dm1_obj
+
+        dm1_name_binding_map = self.dm1_id_name_binding_map[id(dm1_obj)]
+        return self.find_inside_mapping_dm1(ref, dm1_obj, dm1_name_binding_map)
+
+    def find_in_scope_dm1(self, ref: dm1.Ref, dm1_obj: dm1.Object) -> Any:
+        if not ref:
+            return dm1_obj
+
         dm1_closure = self.dm1_id_closure_map[id(dm1_obj)]
         dm1_name_binding_map = self.dm1_id_name_binding_map[id(dm1_obj)]
+        internal_name_map = dm1_closure.new_child(dm1_name_binding_map)
 
-        internal_dm1_name_map = dm1_closure.new_child(dm1_name_binding_map)
+        return self.find_inside_mapping_dm1(ref, dm1_obj, internal_name_map)
 
-        return self.dm1_id_object_map[id(internal_dm1_name_map[ref])]
+    def visit_roots(self, dm1_objs: Iterable[dm1.Object]) -> Iterable[Object]:
+        builtins_closure = collections.ChainMap(BUILTINS)
+        for dm1_obj in dm1_objs:
+            yield self.visit_dm1_Object(dm1_obj, builtins_closure)
 
     def visit_generic(self, thing: Any, dm2_closure: collections.ChainMap) -> Any:
         """
         Handle visiting something - it'll work out where to dispatch the call.
         """
-        match thing:
+        match type(thing):
             case dm1.Object:
                 return self.visit_dm1_Object(thing, dm2_closure)
             case dm1.Link:
@@ -183,7 +237,9 @@ class Lofty:
         """
         return thing
 
-    def visit_dm1_Object(self, dm1_obj: dm1.Object, dm2_closure: collections.ChainMap) -> Object:
+    def visit_dm1_Object(
+        self, dm1_obj: dm1.Object, dm2_closure: collections.ChainMap
+    ) -> Object:
         """
         Handle visiting a dm1.Object
         """
@@ -192,33 +248,42 @@ class Lofty:
         # find the objects representing the supers
         # FIXME: this should do an actual lookup, not just a flat/dumb ref-based one
         # this should only be an issue with multi-part refs
-        dm2_obj.supers = tuple(self.lookup_dm2_obj_by_ref[dm1_obj, super_ref] for super_ref in dm1_obj.supers)
-        dm2_obj.locals_ = tuple((name, self.visit_generic(obj)) for name, obj in dm1_obj.locals_)
-        dm2_obj.name_bindings = dict(filter(lambda x: x[0] is not None, dm2_obj.locals_))
+        dm2_obj.supers = tuple(
+            self.lookup_dm2_obj_by_ref(dm1_obj, super_ref)
+            for super_ref in dm1_obj.supers
+        )
+        dm2_obj.name_bindings = {}
         dm2_obj.closure = dm2_closure.new_child(dm2_obj.name_bindings)
+        dm2_obj.locals_ = tuple(
+            (name, self.visit_generic(obj, dm2_obj.closure)) for name, obj in dm1_obj.locals_
+        )
+        dm2_obj.name_bindings.update(
+            dict(
+                filter(lambda x: x[0] is not None, dm2_obj.locals_)
+            )
+        )
+        return dm2_obj
 
-    def visit_dm1_Link(self, dm1_thing: dm1.Link, dm2_closure: collections.ChainMap) -> Link:
+    def visit_dm1_Link(
+        self, dm1_thing: dm1.Link, dm2_closure: collections.ChainMap
+    ) -> Link:
         """
         Handle visiting a dm1.Link
         """
         raise NotImplementedError
-        return Link(
 
-        )
-
-    def visit_dm1_Replace(self, dm1_thing: dm1.Replace, dm2_closure: collections.ChainMap) -> Replace:
+    def visit_dm1_Replace(
+        self, dm1_thing: dm1.Replace, dm2_closure: collections.ChainMap
+    ) -> Replace:
         """
         Handle visiting a dm1.Replace
         """
-        return Replace()
+        raise NotImplementedError
 
-    def visit_dm1_Import(self, dm1_thing: dm1.Import, dm2_closure: collections.ChainMap) -> Import:
+    def visit_dm1_Import(
+        self, dm1_thing: dm1.Import, dm2_closure: collections.ChainMap
+    ) -> Import:
         """
         Handle visiting a dm1.Import
         """
-
-
-class Scoop:
-    """
-    Scoop's job is to come through and build all the name-binding maps and attach them to all the dm2 objects
-    """
+        raise NotImplementedError
--- tests/model2/test_datamodel2_name_resolution.py
+++ tests/model2/test_datamodel2_name_resolution.py
@@ -1,25 +1,57 @@
-from atopile.dev.parse import parse_as_file, make_parser
-from atopile.model2.datamodel1 import (
-    Object, Link, Import, Dizzy, Type,
-    Replace, MODULE, COMPONENT, PIN, SIGNAL, INTERFACE
-)
-from atopile.parser.AtopileParserVisitor import AtopileParserVisitor
-from atopile.model2.datamodel2 import Scoop, Lofty
-from atopile.model2 import errors
+from contextlib import nullcontext
+from typing import Any, Optional
 from unittest.mock import MagicMock
+
 import pytest
+from atopile.dev.parse import make_parser, parse_as_file
+from atopile.model2 import datamodel1 as dm1
+from atopile.model2 import errors
+from atopile.model2.datamodel2 import Lofty, Scoop
+from atopile.parser.AtopileParserVisitor import AtopileParserVisitor
 
 
-def test_():
+@pytest.mark.parametrize(
+    (   "scope"    , "ref"          , "abs_ref"      , "value", "ex"),
+    (
+        (None      , tuple()        , tuple()        , None   , None     ),  # empty refs point to self
+        (None      , ("z",)         , None           , None   , KeyError ),  # unbound name
+        (None      , ("h",)         , ("h",)         , None   , None     ),  # bound name
+        (None      , ("h", "i")     , None           , None   , KeyError ),  # nested unbound name
+        (None      , ("a", "b", "c"), ("a", "b", "c"), None   , None     ),  # nested bound name
+        (None      , ("i", "j")     , None           , None   , TypeError),  # bound name in global scope
+        (("a",)    , ("b",)         , ("a", "b")     , None   , None     ),  # bound name in parent scope
+        (("a", "b"), ("c",)         , ("a", "b", "c"), None   , None     ),  # bound name in nested parent scope
+        (("a", "b"), ("e",)         , ("e",)         , None   , None     ),  # bound name in closure
+        (("a", "b"), ("e", "f")     , ("e", "f")     , None   , None     ),  # nested bound name in closure
+        (("e",)    , ("a", "b", "c"), None           , 10     , None     ),  # overriden bound name in closure
+        (("e",)    , ("a", "b")     , ("a", "b")     , None   , None     ),  # overriden bound name in closure
+        (("e", "p"), ("a", "b", "c"), None           , 10     , None     ),  # overriden bound name in closure
+    ),
+)
+def test_in_scope(
+    scope: dm1.Ref,
+    ref: dm1.Ref,
+    abs_ref: dm1.Ref,
+    value: Optional[Any],
+    ex: Optional[Exception],
+):
     tree = parse_as_file(
         """
-        component comp1:
-            signal a
-        component comp2 from comp1:
-            signal b
+        module a:
+            module b:
+                signal c
+                signal d
+        module e:
+            signal f
+            signal g
+            a.b.c = 10
+            module p:
+                signal q
+        signal h
+        i = 10
         """
     )
-    dizzy = Dizzy("test.ato")
+    dizzy = dm1.Dizzy("test.ato")
     dm1_tree = dizzy.visitFile_input(tree)
 
     scoop = Scoop()
@@ -32,5 +64,19 @@ def test_():
         search_paths=[],
         path_dm1_map={"test.ato": dm1_tree},
     )
-    dm2_tree = list(lofty.visit_roots([dm1_tree]))[0]
-    dm2_tree
+
+    scope_obj = dm1_tree if scope is None else lofty.find_in_scope_dm1(scope, dm1_tree)
+
+    if ex is not None:
+        with pytest.raises(ex):
+            lofty.find_in_scope_dm1(ref, scope_obj)
+        return  # we pass here
+    else:
+        val = lofty.find_in_scope_dm1(ref, scope_obj)
+
+    if abs_ref is not None:
+        assert val is lofty.find_in_scope_dm1(abs_ref, dm1_tree)
+    elif value is not None:
+        assert val == value
+    else:
+        raise ValueError("bad test case")
