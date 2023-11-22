729d11cd8bf44cca4b4bd2c3b1f08587149d2b11
napowderly <napowderly@gmail.com>
2023-11-22 14:14:24 -0800

    removed push to git feature

--- src/atopile/cli/create.py
+++ src/atopile/cli/create.py
@@ -13,8 +13,7 @@ log.setLevel(logging.INFO)
 
 @click.command()
 @click.argument("name")
-@click.option("--blank", is_flag=True)
-def create(name: str, blank: bool):
+def create(name: str):
     """
     Create a new project from the atopile project template. eg. `ato create my_project`
     """
@@ -69,27 +68,11 @@ def create(name: str, blank: bool):
     with open(project_path / "elec/src/ato.yaml", "w") as stream:
         yaml.safe_dump(data, stream, default_flow_style=False)
 
-
-    # If blank is True, update the content of the .ato file to a blank module
-    if blank:
-        log.info("Blank project selected. Removing template code.")
-        with open(project_path / f"elec/src/{file_name}" , "w") as stream:
-            stream.write(f"module {project_name}:\n")
+    # Create a new module definition within the project ato file
+    with open(project_path / f"elec/src/{file_name}" , "w") as stream:
+        stream.write(f"module {project_name}:\n")
 
     make_initial_commit(project_path)
-
-    # After all changes are done locally, create a new repo on GitLab
-    new_repo_url = (
-        f"https://gitlab.atopile.io/atopile/{project_name}.git"
-    )  # Placeholder URL
-    try:
-        # Now, change the remote URL to the new repository and push the changes
-        push_to_new_repo(project_path, new_repo_url)
-    except Exception as e:
-        log.error(f"Failed to push to new repository: {e}")
-        log.info(
-            "We recommend you find a new home for your project and push it there manually."
-        )
 
 
 def pascal_case(name: str):
@@ -127,19 +110,6 @@ def rename_files(project_dir, new_base_name):
                 new_path = Path(dirpath) / new_name
                 old_path.rename(new_path)
                 log.debug(f"Renamed {old_path} to {new_path}")
-
-
-def push_to_new_repo(local_repo_path, new_repo_url):
-    repo = Repo(local_repo_path)
-    repo.git.remote("set-url", "origin", new_repo_url)
-
-    # Check the current branch name instead of assuming it's 'master'
-    current_branch = repo.active_branch.name
-
-    # Push the current branch and set it to track the remote branch
-    repo.git.push("origin", current_branch, "-u")
-    log.info(f"Pushed to new repository: {new_repo_url}")
-
 
 def make_initial_commit(repo_path, message="Initial commit"):
     repo = Repo(repo_path)
